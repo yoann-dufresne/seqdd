2f1bb5c79750c437e0adf186f22beb374005b046
yoann-dufresne <yoann.dufresne0@gmail.com>
2024-07-26 16:42:37 +0200

    :construction: symlink problems

--- seqdd/utils/download.py
+++ seqdd/utils/download.py
@@ -20,11 +20,14 @@ class DownloadManager:
         print(ncbi)
 
     def get_downloaders(self, bindir):
-        self.datasets = check_binary('datasets', bindir, download_datasets_software)
-        sra = None
-        wget = None
+        self.ncbi = self.sra = wget = None
 
-        return self.datasets, sra, wget
+        if len(self.register.subregisters['ncbi']) > 0:
+            self.ncbi = check_binaries(['datasets'], bindir, download_datasets_software)
+        if len(self.register.subregisters['sra']) > 0:
+            self.sra = check_binaries(['prefetch', 'fasterq-dump'], bindir, download_sra_toolkit)
+
+        return self.ncbi, self.sra, wget
 
     def download_to(self, datadir):
         manager = JobManager(max_process=1)
@@ -36,15 +39,21 @@ class DownloadManager:
         # --- ncbi genomes ---
         ncbi_reg = self.register.subregisters['ncbi']
         if len(ncbi_reg) > 0:
-            downloadable = self.datasets is not None
+            downloadable = self.ncbi is not None
             if downloadable:
-                manager.add_processes(ncbi_jobs_from_accessions(ncbi_reg, datadir, self.datasets))
+                manager.add_processes(ncbi_jobs_from_accessions(ncbi_reg, datadir, self.ncbi['datasets']))
 
             else:
                 print(f'ncbi genomes cannot be downloaded because the datasets tool is absent from the system. Skipping {len(ncbi_reg)} datasets.', file=stderr)
 
         # --- sra datasets ---
-        # TODO
+        sra_reg = self.register.subregisters['sra']
+        if len(sra_reg) > 0:
+            downloadable = self.sra is not None
+            if downloadable:
+                manager.add_processes(sra_jobs_from_accessions(sra_reg, datadir, self.sra))
+            else:
+                print(f'SRA data cannot be downloaded because the sra-tools are absent from the system. Skipping {len(sra_reg)} datasets.', file=stderr)
 
         # --- wget files ---
         # TODO
@@ -59,42 +68,58 @@ class DownloadManager:
 
 # -------------------- Utils downloads --------------------
 
-def check_binary(binary_name, bindir, download_function):
+def check_binaries(binary_names, bindir, download_function):
     """ Look for the binary presence and download it if not present
-    :param: binary_name Name of the binary
+    :param: binary_name Array of searched binaries
     :param: bindir Folder where the binary has to be download if needed
     :param: download_function function triggered if the binary have to be downloaded. the bindir will be given as parameter. True is expected as return on success.
 
     :return: address of the binary. None if not present and not downloadable.
     """
 
-    # Global install
-    try:
-        if subprocess.run(f'{binary_name} --version'.split(' ')) == 0:
-            return binary_name
-    except FileNotFoundError:
-        print(f'{binary_name} not installed on the system. Checking locally...')
+    installed = {name:False for name in binary_names}
+    binaries = {}
 
-    # Local install
-    local_bin = path.join(bindir, binary_name)
-    # Check if not already present
-    if not path.isfile(local_bin):
-        print(f'{binary_name} is not installed locally. Try local automatic installation...')
-        # Automatic download
-        binpath = download_function(bindir)
-        # Failed download
-        if binpath is None:
-            return None
+    for binary_name in binary_names:
+        # Global install
+        try:
+            if subprocess.run(f'{binary_name} --version'.split(' ')) == 0:
+                binaries[binary_name] = binary_name
+                installed[binary_name] = True
+                continue
+        except FileNotFoundError:
+            print(f'{binary_name} not installed on the system. Checking locally...')
 
+        # Local install
+        local_bin = path.join(bindir, binary_name)
+        # Check if not already present
+        if not path.isfile(local_bin):
+            print(f'{binary_name} is not installed locally. Automatic installation will be tried...')
+        else:
+            binaries[binary_name] = local_bin
+            installed[binary_name] = True
+
+    # Return binaries if everything has been found
+    if all(installed.values()):
+        return binaries
+
+    # Automatic download
+    binpaths = download_function(bindir)
+    if binpaths is None:
+        return None
+
+    print(binpaths)
+
+    for name, local_bin in binpaths.items():
     # Exec locally installed binary
-    try:
-        ret = subprocess.run(f'{local_bin} --version'.split(' '))
-        if ret.returncode == 0:
-            return local_bin
-    except FileNotFoundError:
-        print(f'{binary_name} not installed locally. Try automatic local install...')
+        try:
+            ret = subprocess.run(f'{local_bin} --version'.split(' '))
+            if ret.returncode == 0:
+                binaries[name] = local_bin
+        except FileNotFoundError:
+            print(f'{name} still not installed locally. An error has occured during the download process.')
 
-    return None
+    return binaries
 
 
 # -------------------- NCBI genome downloads --------------------
@@ -134,7 +159,7 @@ def download_datasets_software(dest_dir):
 
         ret = subprocess.run(f'chmod +x {binpath}'.split())
         if ret.returncode == 0:
-            return binpath
+            return {'datasets': binpath}
         else:
             print(f'Impossible to change the exec rights for {binpath}. Automatic download of ncbi datasets cli is aborted. Please install it by yourself.', file=stderr)
     else:
@@ -194,6 +219,48 @@ def ncbi_clean(archive, unzip_dir, dest_dir):
 # # -------------------- SRA downloads --------------------
 
 
+def sra_jobs_from_accessions(accessions, datadir, binaries):
+    jobs = []
+
+    # Each dataset download is independant
+    for acc in accessions:
+        # Prefetch data
+        cmd = f'{binaries["prefetch"]} --max-size u --output-directory {datadir} {acc}'
+        prefetch_job = CmdLineJob(cmd)
+
+        # Split files
+        accession_dir = path.join(datadir, acc)
+        cmd = f'{binaries["fasterq-dump"]} --split-3 --skip-technical --outdir {datadir} {accession_dir}'
+        fasterqdump_job = CmdLineJob(cmd, parents=[prefetch_job])
+        
+        # Compress files
+        cmd = f'gzip {path.join(datadir, acc, "*.fastq")}'
+        compress_job = CmdLineJob(cmd, parents=[fasterqdump_job])
+
+        # Move to datadir and clean
+        clean_job = FunctionJob(sra_move_and_clean, func_args=(accession_dir, datadir), parents=[compress_job])
+
+        # Set the jobs
+        jobs.extends((prefetch_job, fasterqdump_job, compress_job, clean_job))
+
+    return jobs
+
+
+def sra_move_and_clean(accession_dir, outdir):
+    # Enumarates all the files from the accession directory
+    for filename in listdir:
+        if filename.endswith('.gz'):
+            rename(path.join(accession_dir, filename), path.join(outdir, filename))
+
+    # Clean the directory
+    rmtree(accession_dir)
+
+# --- Cmds ---
+# ./sratoolkit.3.1.1-ubuntu64/bin/prefetch --max-size u --output-directory outtest SRR000001
+# ./sratoolkit.3.1.1-ubuntu64/bin/fasterq-dump --split-3 --skip-technical --outdir outtest outtest/SRR000001
+
+
+
 def download_sra_toolkit(dest_dir, version='3.1.1'):
     download_link = ''
     dirname = ''
@@ -211,7 +278,7 @@ def download_sra_toolkit(dest_dir, version='3.1.1'):
 
     # Message to potential system extensions
     if not supported:
-        print('sratoolkit auto-install is not yet supported on your system. Plese install the toolkit by yourself. Also maybe we can include your system in the auto-installer. Please submit an issue on github with the following values:', file=stderr)
+        print('sratoolkit auto-install is not yet supported on your system. SRA downloader has not been installed... Also maybe we can include your system in the auto-installer. Please submit an issue on github with the following values:', file=stderr)
         print(f'system={system}\tplateform={platform.machine()}', file=stderr)
         return None
 
@@ -220,26 +287,39 @@ def download_sra_toolkit(dest_dir, version='3.1.1'):
     ret = subprocess.run(cmd.split())
 
     if ret.returncode != 0:
-        print('Impossible to automatically download sratoolkit. Please install it by yourself.', file=stderr)
+        print('Impossible to automatically download sratoolkit. SRA downloader has not been installed...', file=stderr)
         return None
 
     # Uncompress the archive
-    cmd = f'tar -xzf {dirname}.tar.gz'
+    archive_path = path.join(dest_dir, f'{dirname}.tar.gz')
+    cmd = f'tar -xzf {archive_path} -C {dest_dir}'
     ret = subprocess.run(cmd.split())
+    remove(archive_path)
 
     if ret.returncode != 0:
-        print('Impossible to expand the sratoolkit tar.gz on your system. Please install the toolkit yourself.', file=stderr)
+        print('Impossible to expand the sratoolkit tar.gz on your system.', file=stderr)
         return None
 
     # Create links to the bins
     prefetch_bin = path.join(dest_dir, dirname, 'bin', 'prefetch')
-    cmd = f'ln -s {prefetch_bin} {path.join(dest_dir, 'prefetch')}'
+    prefetch_ln = path.join(dest_dir, 'prefetch')
+    cmd = f'ln -s {prefetch_bin} {prefetch_ln}'
     ret = subprocess.run(cmd.split())
-    # TODO: msg
+    if ret.returncode != 0:
+        print(f'Impossible to create symbolic link {prefetch_ln}. SRA downloader has not been installed...', file=stderr)
+        return None
 
     fasterqdump_bin = path.join(dest_dir, dirname, 'bin', 'fasterq-dump')
-    cmd = f'ln -s {prefetch_bin} {path.join(dest_dir, 'fasterq-dump')}'
+    fasterqdump_ln = path.join(dest_dir, 'fasterq-dump')
+    cmd = f'ln -s {prefetch_bin} {fasterqdump_ln}'
     ret = subprocess.run(cmd.split())
-    # TODO: msg
+    if ret.returncode != 0:
+        print(f'Impossible to create symbolic link {fasterqdump_ln}. SRA downloader has not been installed...', file=stderr)
+        return None
 
-    return None
+    return {
+        'prefetch' : prefetch_ln,
+        'fasterq-dump' : fasterqdump_ln
+    }
+
+
