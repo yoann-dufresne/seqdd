318d730055f2fe727fa9fecb6f87cb0a0f70dff3
Bertrand Néron <bneron@pasteur.fr>
2024-11-21 15:42:24 +0100

    :pencil2: configure sphinx

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,18 +5,20 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+print(f"#### {sys.path = }")
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SeqDD'
-copyright = '2024, Yohann Dufresne'
+copyright = '2024, Institut Pasteur (Paris), CNRS'
 author = 'Yohann Dufresne'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx.ext.autodoc',
+              'sphinx_autodoc_typehints',
               'sphinx.ext.todo',
               'sphinx.ext.viewcode',]
 
@@ -30,3 +32,9 @@ exclude_patterns = []
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme_options = {
+    'github_banner': True,
+    'github_user': 'bneron',
+    'github_repo': 'seqdd'
+
+}
